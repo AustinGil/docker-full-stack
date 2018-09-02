92d9298314c105fdfe599fe2684888fbe527c762
Austin Gil <austin@stegosource.com>
2018-09-01 18:34:40 -0700

    Reorganize file

--- models/User.js
+++ models/User.js
@@ -18,6 +18,14 @@ const hashPassword = password => {
   );
 };
 
+const createToken = () => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(16, (err, data) => {
+      err ? reject(err) : resolve(data.toString("base64"));
+    });
+  });
+};
+
 const createUser = user => {
   return database("users")
     .insert(user)
@@ -27,14 +35,6 @@ const createUser = user => {
         ...user
       };
     });
-};
-
-const createToken = () => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(16, (err, data) => {
-      err ? reject(err) : resolve(data.toString("base64"));
-    });
-  });
 };
 
 const register = (request, response) => {
