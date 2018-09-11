d6b791dda09b8f1fe9a5a4ed3047edde2294aea5
Austin Gil <austin@stegosource.com>
2018-09-11 15:04:24 -0700

    Updates to data locations

--- certbot/scripts/run-certbot.sh
+++ certbot/scripts/run-certbot.sh
@@ -2,7 +2,7 @@
 
 # certbot certonly --webroot -w /etc/letsencrypt -d "$DOMAIN" -d "www.$DOMAIN" --agree-tos --email "$EMAIL" --non-interactive --text
 
-certbot certonly --webroot -w /data/letsencrypt --agree-tos --register-unsafely-without-email --non-interactive --text --staging -d "$DOMAIN" -d "www.$DOMAIN"
+certbot certonly --webroot -w /data/letsencrypt --agree-tos --register-unsafely-without-email --non-interactive --text --staging -d "$DOMAIN"
 
-# cp /etc/letsencrypt/archive/"$DOMAIN"/cert1.pem /var/certs/cert1.pem
-# cp /etc/letsencrypt/archive/"$DOMAIN"/privkey1.pem /var/certs/privkey1.pem
+cp /etc/letsencrypt/live/"$DOMAIN"/cert.pem /data/certs/"$DOMAIN"/cert.pem
+cp /etc/letsencrypt/live/"$DOMAIN"/privkey.pem /data/certs/"$DOMAIN"/privkey.pem
