36085df84f78903a179cbe950b30595978dc5f2f
Austin Gil <austin@stegosource.com>
2018-09-11 15:32:35 -0700

    More fixes

--- certbot/scripts/run-certbot.sh
+++ certbot/scripts/run-certbot.sh
@@ -2,6 +2,7 @@
 
 # certbot certonly --webroot -w /etc/letsencrypt -d "$DOMAIN" -d "www.$DOMAIN" --agree-tos --email "$EMAIL" --non-interactive --text
 
-certbot certonly --webroot -w /data/letsencrypt --staging --agree-tos --register-unsafely-without-email --non-interactive --text -d "$DOMAIN" --force-renewal
+# certbot certonly --webroot -w /data/letsencrypt --staging --agree-tos --register-unsafely-without-email --non-interactive --text -d "$DOMAIN" --force-renewal
 
-mkdir -p /data/certs/"$DOMAIN" && cp /etc/letsencrypt/live/"$DOMAIN"/{cert.pem, privkey.pem} "$_"
+# mkdir -p /data/certs/"$DOMAIN"
+# cp /etc/letsencrypt/live/"$DOMAIN"/{cert.pem, privkey.pem} "$_"
