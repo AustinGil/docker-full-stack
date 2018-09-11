0980e8ba3de4f43b9eac44860773d6990557a67b
Austin Gil <austin@stegosource.com>
2018-09-11 15:49:44 -0700

    Fixing domains name for folders

--- certbot/scripts/run-certbot.sh
+++ certbot/scripts/run-certbot.sh
@@ -2,7 +2,9 @@
 
 # certbot certonly --webroot -w /etc/letsencrypt -d "$DOMAIN" -d "www.$DOMAIN" --agree-tos --email "$EMAIL" --non-interactive --text
 
-# certbot certonly --webroot -w /data/letsencrypt --staging --agree-tos --register-unsafely-without-email --non-interactive --text -d "$DOMAIN" --force-renewal
+certbot certonly --webroot -w /data/letsencrypt --staging --agree-tos --register-unsafely-without-email --non-interactive --text -d "$DOMAIN" --force-renewal
 
-# mkdir -p /data/certs/"$DOMAIN"
-# cp /etc/letsencrypt/live/"$DOMAIN"/{cert.pem, privkey.pem} "$_"
+# Create directory with domain name within shared volume
+mkdir -p /data/certs/"$DOMAIN"
+# Move cert files to shared volume at folder name
+cp /etc/letsencrypt/live/"$DOMAIN"/{cert.pem,privkey.pem} "$_"
