9bdeb32e5b7c7261e1e11a46a8486981279ebf22
Austin Gil <austin@stegosource.com>
2018-09-11 15:10:05 -0700

    Changing staging flag

--- certbot/scripts/run-certbot.sh
+++ certbot/scripts/run-certbot.sh
@@ -2,7 +2,7 @@
 
 # certbot certonly --webroot -w /etc/letsencrypt -d "$DOMAIN" -d "www.$DOMAIN" --agree-tos --email "$EMAIL" --non-interactive --text
 
-certbot certonly --webroot -w /data/letsencrypt --agree-tos --register-unsafely-without-email --non-interactive --text --staging -d "$DOMAIN"
+certbot certonly --webroot -w /data/letsencrypt --staging --agree-tos --register-unsafely-without-email --non-interactive --text -d "$DOMAIN"
 
 cp /etc/letsencrypt/live/"$DOMAIN"/cert.pem /data/certs/"$DOMAIN"/cert.pem
 cp /etc/letsencrypt/live/"$DOMAIN"/privkey.pem /data/certs/"$DOMAIN"/privkey.pem
