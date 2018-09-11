0dda6df37023c7cb1ca745c6a9d73f6c2adfdb5f
Austin Gil <austin@stegosource.com>
2018-09-11 14:41:06 -0700

    Testing volume changes

--- certbot/scripts/run-certbot.sh
+++ certbot/scripts/run-certbot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-certbot certonly --webroot -w /data/www -d "$DOMAIN" -d "www.$DOMAIN" --agree-tos --email "$EMAIL" --non-interactive --text
+# certbot certonly --webroot -w /etc/letsencrypt -d "$DOMAIN" -d "www.$DOMAIN" --agree-tos --email "$EMAIL" --non-interactive --text
+
+certbot certonly --webroot -w /data/letsencrypt --agree-tos --register-unsafely-without-email --non-interactive --text --staging -d "$DOMAIN" -d "www.$DOMAIN"
 
 # cp /etc/letsencrypt/archive/"$DOMAIN"/cert1.pem /var/certs/cert1.pem
 # cp /etc/letsencrypt/archive/"$DOMAIN"/privkey1.pem /var/certs/privkey1.pem
