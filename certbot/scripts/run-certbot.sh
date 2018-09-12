114707831cd09d49c4e29f36b1874726d483f428
Austin Gil <austin@stegosource.com>
2018-09-12 10:51:27 -0700

    Certbot going live

--- certbot/scripts/run-certbot.sh
+++ certbot/scripts/run-certbot.sh
@@ -2,7 +2,7 @@
 
 # certbot certonly --webroot -w /etc/letsencrypt -d "$DOMAIN" -d "www.$DOMAIN" --agree-tos --email "$EMAIL" --non-interactive --text
 
-certbot certonly --webroot -w /data/letsencrypt --staging --agree-tos --register-unsafely-without-email --non-interactive --text -d "$DOMAIN" --force-renewal
+certbot certonly --webroot -w /data/letsencrypt --agree-tos --register-unsafely-without-email --non-interactive --text -d "$DOMAIN" --force-renewal # Add --staging flag for testing purposes
 
 # Create directory with domain name within shared volume
 mkdir -p /data/certs/"$DOMAIN"
