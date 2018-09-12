84109a226f4154ba6462913a980b675feb8d1434
Austin Gil <austin@stegosource.com>
2018-09-12 11:00:31 -0700

    Testing certificate change

--- certbot/scripts/run-certbot.sh
+++ certbot/scripts/run-certbot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# certbot certonly --webroot -w /etc/letsencrypt -d "$DOMAIN" -d "www.$DOMAIN" --agree-tos --email "$EMAIL" --non-interactive --text
+# -d "$DOMAIN" -d "www.$DOMAIN"   --non-interactive --text
 
-certbot certonly --webroot -w /data/letsencrypt --agree-tos --register-unsafely-without-email --non-interactive --text -d "$DOMAIN" --force-renewal # Add --staging flag for testing purposes
+certbot certonly --webroot -w /data/letsencrypt --agree-tos --non-interactive --text -d "$DOMAIN" --email "$EMAIL" --force-renewal
+# Add --staging for testing purposes
+# Add --force-renewal to force a renew
 
 # Create directory with domain name within shared volume
 mkdir -p /data/certs/"$DOMAIN"
